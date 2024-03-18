23847e7c127c547b3a644a1343e65c119b5ce9d0
Bill Oatman <woatman@rochester.rr.com>
2024-03-18 13:47:07 -0400

    Ready for release

--- BuildAll.bat
+++ BuildAll.bat
@@ -5,13 +5,13 @@ dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativ
 
 cd \Stuff\Repos\EmbyRefreshLogos\Build
 copy /Y "C:\Stuff\Repos\EmbyRefreshLogos\bin\Release\net8.0\win-x64\publish\EmbyRefreshLogos.exe" .
-"C:\Program Files\7-Zip\7z" a -tzip EmbyRefreshLogos-WIN.zip EmbyRefreshLogos.exe
+"C:\Program Files\7-Zip\7z" a -tzip EmbyRefreshLogos-WIN.zip EmbyRefreshLogos.exe EmbyRefreshLogos.txt
 
 copy /Y "C:\Stuff\Repos\EmbyRefreshLogos\bin\Release\net8.0\linux-arm\publish\EmbyRefreshLogos" .
-"C:\Program Files\7-Zip\7z" a -t7z EmbyRefreshLogos-RasPi.7z EmbyRefreshLogos
+"C:\Program Files\7-Zip\7z" a -t7z EmbyRefreshLogos-RasPi.7z EmbyRefreshLogos EmbyRefreshLogos.txt
 
 copy /Y "C:\Stuff\Repos\EmbyRefreshLogos\bin\Release\net8.0\osx-x64\publish\EmbyRefreshLogos" .
-"C:\Program Files\7-Zip\7z" a -t7z EmbyRefreshLogos-OSX.7z EmbyRefreshLogos
+"C:\Program Files\7-Zip\7z" a -t7z EmbyRefreshLogos-OSX.7z EmbyRefreshLogos EmbyRefreshLogos.txt
 
 copy /Y "C:\Stuff\Repos\EmbyRefreshLogos\bin\Release\net8.0\linux-x64\publish\EmbyRefreshLogos" .
-"C:\Program Files\7-Zip\7z" a -t7z EmbyRefreshLogos-LIN64.7z EmbyRefreshLogos
+"C:\Program Files\7-Zip\7z" a -t7z EmbyRefreshLogos-LIN64.7z EmbyRefreshLogos EmbyRefreshLogos.txt
